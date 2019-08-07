c2d87efa6c13aa6d826f28a10d3e12b4327cae33
TadahiroYamamura <mt.village12@gmail.com>
2019-08-07 12:20:37 +0900

    :送信まで実装

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from email import encoders
-from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
+from email.message import EmailMessage
 from email.utils import formatdate
+from glob import glob
+import mimetypes
 from smtplib import SMTP
 import os
 
@@ -15,31 +16,34 @@ class Mail:
 
 
   def message(self, subject, body):
-    msg = MIMEMultipart(body)
+    msg = EmailMessage()
     msg['Subject'] = subject
+    msg.set_content(body)
     msg['From'] = os.getenv('SEND_FROM')
     msg['To'] = ', '.join(self.to)
     msg['Cc'] = ', '.join(self.cc)
     msg['Date'] = formatdate()
-    return msg
+    self._msg = msg
+    return self
 
 
-  def attach(self, msg, attachment_path):
-    part = MIMEBase('application', 'octet-stream')
-    with open(attachment_path, 'rb') as r:
-      part.set_payload(r.read())
-    encoders.encode_base64(part)
+  def attach(self, attachment_path):
+    mime = mimetypes.guess_type(attachment_path)[0]
+    (mime_main, mime_sub) = mime.split('/')
     filename = os.path.basename(attachment_path)
-    part.add_header('Content-Disposition', 'attachment; filename="{}"'.format(filename))
-    msg.attach(part)
-    return msg
+    with open(attachment_path, 'rb') as r:
+      self._msg.add_attachment(r.read(),
+                         maintype=mime_main,
+                         subtype=mime_sub,
+                         filename=filename)
+    return self
 
 
-  def send(self, msg):
+  def send(self):
     with SMTP(os.getenv('HOST'), os.getenv('PORT')) as smtp:
       smtp.starttls()
       smtp.login(os.getenv('SEND_FROM'), os.getenv('PASSWORD'))
-      smtp.send_message(msg)
+      smtp.send_message(self._msg)
 
 
   @staticmethod
@@ -59,3 +63,25 @@ class Mail:
     me.to = to_list
     me.cc = cc_list
     return me
+
+
+def main():
+  dotenv.load_dotenv()
+  mails = map(lambda path: Mail.from_file(path), glob('data/accounts/*.txt'))
+  print()
+  print('【使用可能なメール文】')
+  for txt in glob('data/texts/*.txt'):
+    print(os.path.basename(txt)[:-4])
+  print()
+  subject = input('どれを送信しますか？：')
+  with open(os.path.join('data/texts', subject + '.txt')) as f:
+    body_message = f.read()
+  mails = map(lambda m: m.message(subject, body_message), mails)
+  for atch in glob('data/attachments/*'):
+    mails = map(lambda m: m.attach(atch), mails)
+  for m in mails:
+    m.send()
+
+
+if __name__ == '__main__':
+  main()
